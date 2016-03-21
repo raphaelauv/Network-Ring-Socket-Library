59fd25602422f0ec5c802e35303dcfe67f2b448b
raphaelauv <raphaelauv@users.noreply.github.com>
2016-03-21 23:14:27 +0100

    Ajout methode envoyerMessage et receptionMessage dans Serv

--- src/Java/Serv.java
+++ src/Java/Serv.java
@@ -1,7 +1,17 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 class SizeException extends Exception{
+}
+
+class AlreadyAllUdpPortSet extends Exception{
+	
 }
 
 public class Serv implements Communication{
@@ -12,59 +22,98 @@ public class Serv implements Communication{
 	private int udp2;
 	private int portTcp;
 	
+	
+	DatagramSocket sender;
+	byte[]dataTosend;
+	
 	private HashMap<Integer, Boolean> IdAlreadyReceve;// hashmap contenant les id deja croisé
-	
-	
 	private LinkedList<Message> listForApply; // liste des message recu qui sont pour cette ID
 	private LinkedList<Message> listToSend;// liste des message a envoyé
 
-	public String lire(){
-		//prend le verrou
+	public String lire() {
+		synchronized (listForApply) {
 
-		while(listForApply.isEmpty()){
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}//se met en attente
+			while (listForApply.isEmpty()) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			}
+			return listForApply.pop().getContenu();
 		}
-		//libere le verrou
-		return listForApply.pop().getContenu();
-	
 	}
 
 	public void envoyer(String message , int id) throws SizeException{
-
-		//envoyer un message
-		
+	
 		if(message.length()>250){
 			throw new SizeException();
 		}
 		
-		Message tmp=new Message(id,message);
-		
-		//prend le verrou
-		
+		synchronized(listToSend){
+			
+			//TODO mettre en forme le message avant d'ajouter dans liste
+			listToSend.add(new Message(id,message));
+			notifyAll();
+		}
 		
 	}
 
+	private void receveMessage(){
+		
+		
+	}
 	
-	public Serv(int a, int id) {
+	private void sendMessage() throws UnknownHostException, InterruptedException{
+		synchronized (listToSend){
+			while(listToSend.isEmpty()){
+				wait();
+			}
+			dataTosend=listToSend.pop().getContenu().getBytes();
+		}
+		
+		if(udp1 !=0){
+			DatagramPacket paquet1=new DatagramPacket(dataTosend,dataTosend.length,InetAddress.getByName("localhost"),udp1);
+			try {
+				sender.send(paquet1);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(udp2 != 0){
+			DatagramPacket paquet2=new DatagramPacket(dataTosend,dataTosend.length,InetAddress.getByName("localhost"),udp2);
+			try {
+				sender.send(paquet2);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		;
+		
+	}
+	
+	public Serv(int a, int id) throws SocketException {
 
 		super();
+		this.sender=new DatagramSocket();
 		this.listToSend=new LinkedList<Message>();
 		this.listToSend=new LinkedList<Message>();
 		this.udp1 = a;
 		this.id = id;
 	}
 
-	public void dedoubler(int b){
+	public void dedoubler(int udpNew) throws AlreadyAllUdpPortSet{
 		if (this.udp2 !=0){
-			this.udp2=b;
+			this.udp2=udpNew;
+		}
+		else if (this.udp1 != 0){
+			this.udp1=udpNew;
 		}
 		else{
-			// TODO
+			throw new AlreadyAllUdpPortSet();
 		}
 	}
 	public int getB() {
