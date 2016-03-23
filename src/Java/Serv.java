2f4b594a8a74d5ec8a0f6a9f0af1a9296fbc62bd
raphaelauv <raphaelauv@users.noreply.github.com>
2016-03-23 20:58:55 +0100

    mise en page

--- src/Java/Serv.java
+++ src/Java/Serv.java
@@ -17,38 +17,37 @@ import java.lang.Runnable;
 
 class SizeException extends Exception {
 }
+
 class AlreadyAllUdpPortSet extends Exception {
 }
-class DOWNmessageException extends Exception{
-	
+
+class DOWNmessageException extends Exception {
+
 }
 
 public class Serv implements Communication {
 	public boolean verboseMode;
 	private String ip;
-	
+
 	private int id;
-	
+
 	private int portTcp;
 	private ServerSocket sockServerTCP;
-	
-	
+
 	private DatagramSocket sockSender;
 	private Integer numberPortUDP1;
 	private String ipPortUDP1;
-	
+
 	private Integer numberPortUDP2;
 	private String ipPortUDP2;
-	
-	
+
 	private DatagramSocket sockRecever;
 	private Integer numberLICENPortUDP;
 
 	private String ipMULTI;
 	private Integer numberPortMULTI;
 	private MulticastSocket sockMultiRECEP;
-	
-	
+
 	private byte[] dataTosend;
 	private byte[] dataToReceve;
 
@@ -72,21 +71,20 @@ public class Serv implements Communication {
 	private Thread ThServTCP;
 	private Thread ThSend1;
 	private Thread ThSend2;
-	
-	
+
 	private Boolean EYBGisArrive;
-	
+
 	private Boolean TESTisComeBack;
 	private Integer ValTEST;
-	
+
 	private boolean isDOWN;
-	
-	private void communicationIsDown() throws DOWNmessageException{
-		if(isDOWN){
+
+	private void communicationIsDown() throws DOWNmessageException {
+		if (isDOWN) {
 			throw new DOWNmessageException();
 		}
 	}
-	
+
 	private void receveMULTI() throws IOException, DOWNmessageException {
 		this.sockMultiRECEP = new MulticastSocket(this.numberPortMULTI);
 		this.sockMultiRECEP.joinGroup(InetAddress.getByName(ipMULTI.toString()));
@@ -111,135 +109,155 @@ public class Serv implements Communication {
 
 	}
 
-	public void test() throws InterruptedException, DOWNmessageException{
+	public void test() throws InterruptedException, DOWNmessageException {
 		communicationIsDown();
-		Integer idMessage=100;
-		String test="TEST"+" "+idMessage+" "+this.ipMULTI+" "+this.numberPortMULTI;
-		
-		Message q=new Message(8, test);
-		
-		
-		this.ValTEST=idMessage;
-		
-		
-		this.TESTisComeBack=false;
-		
+		Integer idMessage = 100;
+		String test = "TEST" + " " + idMessage + " " + this.ipMULTI + " " + this.numberPortMULTI;
+
+		Message q = new Message(8, test);
+
+		this.ValTEST = idMessage;
+
+		this.TESTisComeBack = false;
+
 		synchronized (listToSend) {
 			listToSend.add(q);
 		}
-		
+
 		wait(2000);
-		
-		if(!TESTisComeBack){
-			if(verboseMode){System.out.println("message TEST is NOT comeback");}
-			//TODO
+
+		if (!TESTisComeBack) {
+			if (verboseMode) {
+				System.out.println("message TEST is NOT comeback");
+			}
+			// TODO
 		}
-		if(verboseMode){System.out.println("message TEST is comeback");}
-		
+		if (verboseMode) {
+			System.out.println("message TEST is comeback");
+		}
+
 	}
-	
+
 	public void quitter() throws InterruptedException, DOWNmessageException {
 		communicationIsDown();
-		Integer idMessage=100;
-		
-		String quit="GBYE"+" "+idMessage+" "+this.ip+" "+this.numberLICENPortUDP+" "+this.ipPortUDP1+" "+this.numberPortUDP1;
-		
-		Message q=new Message(8, quit);
-		EYBGisArrive=false;
+		Integer idMessage = 100;
+
+		String quit = "GBYE" + " " + idMessage + " " + this.ip + " " + this.numberLICENPortUDP + " " + this.ipPortUDP1
+				+ " " + this.numberPortUDP1;
+
+		Message q = new Message(8, quit);
+		EYBGisArrive = false;
 		synchronized (listToSend) {
 			listToSend.add(q);
 		}
-		
+
 		synchronized (EYBGisArrive) {
-			while(!EYBGisArrive){
+			while (!EYBGisArrive) {
 				EYBGisArrive.wait();
 			}
 		}
-		//TODO deconnect;
-		
+		// TODO deconnect;
+
 	}
-	
-	public void connectTo(String adresse, int idTCP) throws AlreadyAllUdpPortSet, UnknownHostException, IOException, DOWNmessageException {
+
+	public void connectTo(String adresse, int idTCP)
+			throws AlreadyAllUdpPortSet, UnknownHostException, IOException, DOWNmessageException {
 		communicationIsDown();
-		if(numberPortUDP1 != 0){
+		if (numberPortUDP1 != 0) {
 			throw new AlreadyAllUdpPortSet();
 		}
-		
-		synchronized (sockServerTCP) {//pas d'acceptation de connection pendant une connection
-			
-			Socket socket=new Socket(adresse,idTCP);
-			if(verboseMode){System.out.println("conecter en TCP a :"+adresse+" sur port : "+idTCP);}
-			
-			BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter pw=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-			String m1=br.readLine();
-			
-			if(verboseMode){System.out.println("message RECEVE "+m1);}
-			
-			String m2="NEWC"+" "+this.ip+" "+this.numberPortUDP1+"\n";
-			
+
+		synchronized (sockServerTCP) {// pas d'acceptation de connection pendant
+										// une connection
+
+			Socket socket = new Socket(adresse, idTCP);
+			if (verboseMode) {
+				System.out.println("conecter en TCP a :" + adresse + " sur port : " + idTCP);
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			String m1 = br.readLine();
+
+			if (verboseMode) {
+				System.out.println("message RECEVE " + m1);
+			}
+
+			String m2 = "NEWC" + " " + this.ip + " " + this.numberPortUDP1 + "\n";
+
 			pw.print(m2);
 			pw.flush();
-			if(verboseMode){System.out.println("message SEND "+m2);}
-			
-			String m3=br.readLine();
-			if(verboseMode){System.out.println("message RECEVE "+m3);}
+			if (verboseMode) {
+				System.out.println("message SEND " + m2);
+			}
+
+			String m3 = br.readLine();
+			if (verboseMode) {
+				System.out.println("message RECEVE " + m3);
+			}
 			pw.close();
 			br.close();
 			socket.close();
-			
-			
-			}
+
 		}
-		
-	
+	}
+
 	/**
 	 * Serv in TCP to accept an entrance TCP connection
-	 * @param idTCP port TCP of serv
+	 * 
+	 * @param idTCP
+	 *            port TCP of serv
 	 * @throws IOException
 	 */
 	private void servTCP() throws IOException {
-		
+
 		synchronized (sockServerTCP) {
-		
-			this.sockServerTCP=new ServerSocket(this.portTcp);
-			
-			Socket socket=sockServerTCP.accept();
-			
-			if(verboseMode){System.out.println("TCP connect");
-			
+
+			this.sockServerTCP = new ServerSocket(this.portTcp);
+
+			Socket socket = sockServerTCP.accept();
+
+			if (verboseMode) {
+				System.out.println("TCP connect");
+
 			}
-			BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter pw=new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-			
-			
-			String m1="WELC"+" "+this.ip+" "+this.numberLICENPortUDP+" "+this.ipMULTI+" "+this.numberPortMULTI+"\n";
-			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+			String m1 = "WELC" + " " + this.ip + " " + this.numberLICENPortUDP + " " + this.ipMULTI + " "
+					+ this.numberPortMULTI + "\n";
+
 			pw.print(m1);
-			pw.flush(); 
-			
-			if(verboseMode){System.out.println("TCP : message SEND: "+m1);}
-			String m2=br.readLine();
-			
-			if(verboseMode){System.out.println("TCP : message RECEVE : "+m2);}
-			
+			pw.flush();
+
+			if (verboseMode) {
+				System.out.println("TCP : message SEND: " + m1);
+			}
+			String m2 = br.readLine();
+
+			if (verboseMode) {
+				System.out.println("TCP : message RECEVE : " + m2);
+			}
+
 			pw.print("ACKC\n");
 			pw.flush();
-			
-			if(verboseMode){System.out.println("TCP : message SEND: ");}
-			
+
+			if (verboseMode) {
+				System.out.println("TCP : message SEND: ");
+			}
+
 			pw.close();
 			br.close();
 			socket.close();
-			
+
 			synchronized (numberPortUDP1) {
-				
+
 			}
 		}
 	}
-	
+
 	public String lire() throws DOWNmessageException {
-		
+
 		communicationIsDown();
 		synchronized (listForApply) {
 
@@ -263,9 +281,9 @@ public class Serv implements Communication {
 		}
 
 		synchronized (listToSend) {
-			
+
 			System.out.println("je suis dans methode envoyer");
-			
+
 			// TODO mettre en forme le message avant d'ajouter dans liste
 			this.listToSend.add(new Message(id, message));
 			this.listToSend.notifyAll();
@@ -294,26 +312,24 @@ public class Serv implements Communication {
 			System.out.println("Message Recu : " + st);
 		}
 
-		Integer idm=100;
-		
+		Integer idm = 100;
+
 		if (st.startsWith("GBYE")) {
 			String m = "EYBG" + " " + idm;
 
 		}
 
 		else if (st.startsWith("TEST")) {
-			if(st.substring(4).startsWith(ValTEST.toString())){				
-				this.TESTisComeBack=true;
+			if (st.substring(4).startsWith(ValTEST.toString())) {
+				this.TESTisComeBack = true;
 			}
-		}
-		else if (st.startsWith("EYBG")) {
+		} else if (st.startsWith("EYBG")) {
 
 			synchronized (EYBGisArrive) {
 				EYBGisArrive = true;
 				EYBGisArrive.notifyAll();
 			}
-			
-			
+
 		} else {
 			synchronized (this.listToSend) {
 				this.listToSend.add(tmp);
@@ -366,23 +382,24 @@ public class Serv implements Communication {
 
 	}
 
-	public Serv(boolean verboseMode,Integer numberLICENPortUDP) throws SocketException {
+	public Serv(boolean verboseMode, Integer numberLICENPortUDP) throws SocketException {
 
 		super();
-		this.ipMULTI="225.1.2.4";
-		this.numberPortMULTI=9999;
+		this.ipMULTI = "225.1.2.4";
+		this.numberPortMULTI = 9999;
 		this.sockSender = new DatagramSocket();
-		this.numberLICENPortUDP=numberLICENPortUDP;
+		this.numberLICENPortUDP = numberLICENPortUDP;
 		this.sockRecever = new DatagramSocket(numberLICENPortUDP);
 		this.listToSend = new LinkedList<Message>();
 		this.listForApply = new LinkedList<Message>();
 		this.verboseMode = verboseMode;
-		
+
 		// this.udp1 = udp1;
 		// this.id = id;
 
 		/*******************************************************************
-		 * Creation des 4 thread anonyme :  d'envoi UDP | reception UDP | serv tcp | reception multi
+		 * Creation des 4 thread anonyme : d'envoi UDP | reception UDP | serv
+		 * tcp | reception multi
 		 * 
 		 */
 		this.runRecev = new Runnable() {
@@ -397,10 +414,10 @@ public class Serv implements Communication {
 				}
 			}
 		};
-		
+
 		this.runMULTIRecev = new Runnable() {
 			public void run() {
-				boolean isDown=false;
+				boolean isDown = false;
 				while (!isDown) {
 					try {
 						receveMULTI();
@@ -408,7 +425,7 @@ public class Serv implements Communication {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					} catch (DOWNmessageException e) {
-						isDown=true;
+						isDown = true;
 					}
 				}
 			}
@@ -441,23 +458,22 @@ public class Serv implements Communication {
 			}
 
 		};
-		
+
 		this.ThRecev = new Thread(runRecev);
 		this.ThSend1 = new Thread(runSend1);
-		this.ThServTCP =new Thread(runServTCP);
-		this.ThMULTIrecev=new Thread(runMULTIRecev);
-		
+		this.ThServTCP = new Thread(runServTCP);
+		this.ThMULTIrecev = new Thread(runMULTIRecev);
+
 		this.ThRecev.start();
 		this.ThSend1.start();
 		this.ThServTCP.start();
 		this.ThMULTIrecev.start();
-	
-		//this.ThServTCP.join();
-		//this.ThRecev.join();
-		//this.ThSend1.join();
+
+		// this.ThServTCP.join();
+		// this.ThRecev.join();
+		// this.ThSend1.join();
 
 	}
-
 
 	public void dedoubler(int udpNew) throws AlreadyAllUdpPortSet, InterruptedException {
 
