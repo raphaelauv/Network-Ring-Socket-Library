823d76399968877ad408ff13330454fc880b8295
raphaelauv <raphaelauv@users.noreply.github.com>
2016-03-23 12:38:51 +0100

    Correction des thread annonyme , correction strat pour dedoublement

--- src/Java/Serv.java
+++ src/Java/Serv.java
@@ -8,38 +8,39 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.lang.Runnable;
 
-class SizeException extends Exception{
+class SizeException extends Exception {
 }
 
-class AlreadyAllUdpPortSet extends Exception{
-	
+class AlreadyAllUdpPortSet extends Exception {
+
 }
-public class Serv implements Communication{
-	
-	
+
+public class Serv implements Communication {
+
 	private int id;
-	private int udp1;
-	private int udp2;
+	private int numberPortUDP1;
+	private int numberPortUDP2;
 	private int portTcp;
-	
-	
+
 	DatagramSocket sockSender;
 	DatagramSocket sockRecever;
-	byte[]dataTosend;
-	byte[]dataToReceve;
-	
-	private HashMap<Integer, Boolean> IdAlreadyReceve;// hashmap contenant les id deja croisé
-	private LinkedList<Message> listForApply; // liste des message recu qui sont pour cette ID
+	byte[] dataTosend;
+	byte[] dataToReceve;
+
+	private HashMap<Integer, Boolean> IdAlreadyReceve;// hashmap contenant les
+														// id deja croisé
+	private LinkedList<Message> listForApply; // liste des message recu qui sont
+												// pour cette ID
 	private LinkedList<Message> listToSend;// liste des message a envoyé
-	
+
 	private Runnable runRecev;
 	private Runnable runSend1;
 	private Runnable runSend2;
-	
+
 	private Thread ThRecev;
 	private Thread ThSend1;
 	private Thread ThSend2;
-	
+
 	public String lire() {
 		synchronized (listForApply) {
 
@@ -49,55 +50,56 @@ public class Serv implements Communication{
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} 
+				}
 			}
 			return listForApply.pop().getContenu();
 		}
 	}
 
-	public void envoyer(String message , int id) throws SizeException{
-	
-		if(message.length()>250){
+	public void envoyer(String message, int id) throws SizeException {
+
+		if (message.length() > 250) {
 			throw new SizeException();
 		}
-		
-		synchronized(listToSend){
-			
-			//TODO mettre en forme le message avant d'ajouter dans liste
-			listToSend.add(new Message(id,message));
+
+		synchronized (listToSend) {
+
+			// TODO mettre en forme le message avant d'ajouter dans liste
+			listToSend.add(new Message(id, message));
 			notifyAll();
 		}
-		
+
 	}
 
-	private void receveMessage() throws IOException{
-		
-		this.dataToReceve=new byte[100];
-		DatagramPacket paquet=new DatagramPacket(dataToReceve,dataToReceve.length);
+	private void receveMessage() throws IOException {
+
+		this.dataToReceve = new byte[100];
+		DatagramPacket paquet = new DatagramPacket(dataToReceve, dataToReceve.length);
 		System.out.println("j'attends de recevoir un message dans RECEVE");
 		this.sockRecever.receive(paquet);
-		String st=new String(paquet.getData(),0,paquet.getLength());
+		String st = new String(paquet.getData(), 0, paquet.getLength());
 
 		Message tmp = new Message(10, st);
-		
+		System.out.println(st);
 		synchronized (this.listToSend) {
 			this.listToSend.add(tmp);
 			listToSend.notify();
 		}
-		
+
 	}
-	
-	private  void  sendMessage() throws UnknownHostException, InterruptedException{
-		synchronized (listToSend){
-			while(listToSend.isEmpty()){
+
+	private void sendMessage() throws UnknownHostException, InterruptedException {
+		synchronized (listToSend) {
+			while (listToSend.isEmpty()) {
 				System.out.println("j'attends d'avoir un message a envoyer dans SEND");
 				listToSend.wait();
 			}
-			dataTosend=listToSend.pop().getContenu().getBytes();
+			dataTosend = listToSend.pop().getContenu().getBytes();
 		}
-		
-		if(udp1 !=0){
-			DatagramPacket paquet1=new DatagramPacket(dataTosend,dataTosend.length,InetAddress.getByName("localhost"),udp1);
+
+		if (numberPortUDP1 != 0) {
+			DatagramPacket paquet1 = new DatagramPacket(dataTosend, dataTosend.length,
+					InetAddress.getByName("localhost"), numberPortUDP1);
 			try {
 				sockSender.send(paquet1);
 			} catch (IOException e) {
@@ -105,8 +107,9 @@ public class Serv implements Communication{
 				e.printStackTrace();
 			}
 		}
-		if(udp2 != 0){
-			DatagramPacket paquet2=new DatagramPacket(dataTosend,dataTosend.length,InetAddress.getByName("localhost"),udp2);
+		if (numberPortUDP2 != 0) {
+			DatagramPacket paquet2 = new DatagramPacket(dataTosend, dataTosend.length,
+					InetAddress.getByName("localhost"), numberPortUDP2);
 			try {
 				sockSender.send(paquet2);
 			} catch (IOException e) {
@@ -115,48 +118,47 @@ public class Serv implements Communication{
 			}
 		}
 		;
-		
+
 	}
-	
+
 	public Serv() throws SocketException, InterruptedException {
 
 		super();
 		this.sockSender = new DatagramSocket();
-		this.sockRecever =new DatagramSocket(5555);
+		this.sockRecever = new DatagramSocket(5555);
 		this.listToSend = new LinkedList<Message>();
 		this.listForApply = new LinkedList<Message>();
-		//this.udp1 = udp1;
-		//this.id = id;
+		// this.udp1 = udp1;
+		// this.id = id;
 
 		/*******************************************************************
-		 * Creation des class anonyme propre au thread d'envoi et celui de reception
+		 * Creation des class anonyme propre au thread d'envoi et celui de
+		 * reception
 		 * 
 		 */
 		this.runRecev = new Runnable() {
 			public void run() {
-				while(true){
+				while (true) {
 					try {
-						
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						
-						System.out.println("dans thread send");
-						sendMessage();
-					} catch (UnknownHostException | InterruptedException e) {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+					try {
+						receveMessage();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println("dans thread send");
 				}
 			}
 		};
-		
+
 		this.runSend1 = new Runnable() {
 			public void run() {
-				while(true){
+				while (true) {
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -165,8 +167,8 @@ public class Serv implements Communication{
 					}
 					System.out.println("dans thread receve");
 					try {
-						receveMessage();
-					} catch (IOException e) {
+						sendMessage();
+					} catch (UnknownHostException | InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
@@ -174,30 +176,30 @@ public class Serv implements Communication{
 			}
 		};
 
-		this.ThRecev=new Thread(runRecev);
-		this.ThSend1=new Thread(runSend1);
-		
+		this.ThRecev = new Thread(runRecev);
+		this.ThSend1 = new Thread(runSend1);
+
 		this.ThRecev.start();
 		this.ThSend1.start();
-		
+
 		this.ThRecev.join();
 		this.ThSend1.join();
-		
+
 		System.out.println("fin serv");
-		
+
 	}
 
-	public void dedoubler(int udpNew) throws AlreadyAllUdpPortSet, InterruptedException{
-		
-		if(this.udp1 != 0 && this.udp2 != 0 ){
+	public void dedoubler(int udpNew) throws AlreadyAllUdpPortSet, InterruptedException {
+
+		if (this.numberPortUDP1 != 0 && this.numberPortUDP2 != 0) {
 			throw new AlreadyAllUdpPortSet();
-		}
-		else if (this.udp2 ==0){
-			this.udp2=udpNew;
 			
+		} else if (this.numberPortUDP2 == 0) {
+			this.numberPortUDP2 = udpNew;
+
 			this.runSend2 = new Runnable() {
 				public void run() {
-					while(true){
+					while (true) {
 						try {
 							receveMessage();
 						} catch (IOException e) {
@@ -207,21 +209,22 @@ public class Serv implements Communication{
 					}
 				}
 			};
-			this.ThSend2=new Thread(runSend2);
+			this.ThSend2 = new Thread(runSend2);
+			this.ThSend2.start();
 			this.ThSend2.join();
-		}
-		else if (this.udp1 != 0){
-			this.udp1=udpNew;
-			
-			//todo same que le else if, ameliorer synthaxe du code
+		} else if (this.numberPortUDP1 != 0) {
+			this.numberPortUDP1 = udpNew;
+
+			// todo same que le else if, ameliorer synthaxe du code
 		}
 	}
+
 	public int getB() {
-		return udp2;
+		return numberPortUDP2;
 	}
 
 	public void setB(int b) {
-		this.udp2 = b;
+		this.numberPortUDP2 = b;
 	}
 
 	public int getId() {
@@ -231,6 +234,5 @@ public class Serv implements Communication{
 	public void setId(int id) {
 		this.id = id;
 	}
-
 
 }
