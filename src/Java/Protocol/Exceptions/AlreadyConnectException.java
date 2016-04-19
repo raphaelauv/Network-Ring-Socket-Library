eb629319c9a465ca540e6b965a94e1beac001ba6
raphaelauv <raphaelauv@users.noreply.github.com>
2016-04-20 01:21:15 +0200

    nouvelles Exception de DUPLICATION

---
+++ src/Java/Protocol/Exceptions/AlreadyConnectException.java
@@ -0,0 +1,5 @@
+package Protocol.Exceptions;
+	public class AlreadyConnectException extends Exception{
+		private static final long serialVersionUID = 1L;
+
+}
---
+++ src/Java/Protocol/Exceptions/ImpossibleDUPLConnection.java
@@ -0,0 +1,6 @@
+package Protocol.Exceptions;
+
+public class ImpossibleDUPLConnection  extends Exception{
+	private static final long serialVersionUID = 1L;
+
+}
